f7a6556e0046a04471a5076f6e8e8798e8df9f74
Vikas Minda <47515761+vminda1278@users.noreply.github.com>
2025-08-24 18:03:22 +0530

    Update role claims and add supplier sales executive management endpoints in API collection and environment

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
     // API configuration
     API_URL_PREFIX: process.env.API_URL_PREFIX || '/v1',
     ROLES_CLAIMS: {
-        supplier_sales_rm: [""],
-        supplier_sales_head: [""],
-        supplier_sales_manager: [""],
-        supplier_sales_executive: [""],
+        supplier_sales_rm: [],
+        supplier_sales_head: [],
+        supplier_sales_manager: [],
+        supplier_sales_executive: [],
         supplier_admin: ["supplier:manageUser"],
         superadmin_admin: [
             "superadmin:manage"
